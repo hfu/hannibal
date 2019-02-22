5f336366caaf273bdba924a3937c01fc9bcfbb5a
Hidenori Fujimura <hfu@users.noreply.github.com>
2019-02-22 17:37:55 -0500

    update

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# hannibal
+# hannibal: Chiiki Hanni of Kobe, Japan in two vector tiles
 
-# attribution
+## background
+I wanted to test the idea of hosting a few number of vector tiles as static resources to provide polygon data for map painting.
+
+## the product
+
+## production of vector tiles
+First you need to install [Tippecanoe](https://github.com/mapbox/tippecanoe).
+
+```console
+git clone git@github.com:hfu/hannibal
+cd hannibal
+npm install
+node index.js
+```
+
+## attribution
 [src/chiiki_hanni.zip](http://www.city.kobe.lg.jp/ward/activate/participate/localdata/index.html?fbclid=IwAR0qnOK7QBn-wo2TdWrO9zEYUiU-zdmSZUZwOpHB0fmuykZjj-s1mDLXyD4) is from Kobe, Japan.
 
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const config = require('config')
 const shapefile = require('shapefile')
 const { spawn } = require('child_process')
+const reproject = require('reproject')
 
 const src = config.get('src')
+const def = '+proj=tmerc +lat_0=36 +lon_0=134.3333333333333 +k=0.9999 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs'
 
 const convert = (writable) => {
   return new Promise((resolve, reject) => {
@@ -14,12 +16,18 @@ const convert = (writable) => {
           resolve(null)
         } else {
           let f = result.value
+          f.geometry = reproject.toWgs84(f.geometry, def)
           delete f.properties['SHAPE_Leng']
           delete f.properties['SHAPE_Area']
           f.properties.name = f.properties.NAME
           delete f.properties['NAME']
           f.properties.f_no = f.properties.F_NO
           delete f.properties['F_NO']
+          f.tippecanoe = {
+            layer: 'hani',
+            minzoom: 8,
+            maxzoom: 8
+          }
           writable.write(`\x1e${JSON.stringify(f)}\n`)
           return source.read().then(filter)
         }
